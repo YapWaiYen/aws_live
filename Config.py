1a012782b83f55e50bd3e27823680b83cc4de32b
YapWaiYen <114348915+YapWaiYen@users.noreply.github.com>
2022-09-27 22:42:20 +0800

    Add files via upload

--- Config.py
+++ Config.py
@@ -1,6 +1,6 @@
-customhost = "employee.clll3rm4d9fx.us-east-1.rds.amazonaws.com"  
+customhost = "employee.ctwfgijxruex.us-east-1.rds.amazonaws.com"  
 customuser = "aws_user"
 custompass = "bait3273"
 customdb = "employee" 
-custombucket = "leeyixuan"
+custombucket = "yapwaiyen-employee"
 customregion = "us-east-1" 
