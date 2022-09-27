9ed7bd19e861674880e043c495f29dfc2c2619fb
YapWaiYen <114348915+YapWaiYen@users.noreply.github.com>
2022-09-27 23:28:34 +0800

    Delete Config.py

--- Config.py
+++
@@ -1,6 +0,0 @@
-customhost = "employee.clll3rm4d9fx.us-east-1.rds.amazonaws.com"  
-customuser = "aws_user"
-custompass = "bait3273"
-customdb = "employee" 
-custombucket = "leeyixuan"
-customregion = "us-east-1" 
